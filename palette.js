d789bd8ea5308878cff59975619014ddcbecd396
Sebastien Luong <sebastienluong@gmail.com>
2016-07-08 18:32:24 -0700

    Draw line on palette

---
+++ palette.js
@@ -0,0 +1,71 @@
+"use strict"
+
+function draw() {
+let context = document.getElementById('canvasInAPerfectWorld').
+               getContext("2d");
+let canvas = document.getElementById('canvasInAPerfectWorld');
+let clickX = new Array();
+let clickY = new Array();
+let clickDrag = new Array();
+let paint;
+
+canvas.onmousedown = function( e ){
+  let mouseX = e.pageX - this.offsetLeft;
+  let mouseY = e.pageY - this.offsetTop;
+
+  paint = true;
+
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
+
+  redraw();
+};
+
+canvas.onmousemove = function( e ){
+  if(paint) {
+
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+};
+
+canvas.onmouseup = function( e ){
+  paint = false;
+};
+
+canvas.onmouseleave = function( e ){
+  paint = false;
+};
+
+function addClick(x, y, dragging){
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw(){
+  // clears canvas
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  context.strokeStyle = "#df4b26";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+
+  for( let i=0; i< clickX.length; i++){
+    context.beginPath();
+
+    if (clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+    }
+    else {
+      context.moveTo(clickX[i] - 1, clickY[i]);
+    }
+
+    context.lineTo(clickX[i], clickY[i]);
+    context.closePath();
+    context.stroke();
+  }
+}
+
+}
+
+
